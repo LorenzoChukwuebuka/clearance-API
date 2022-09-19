6235d0196255ae8c6dc67440f53028fe7ffc24ce
LorenzoChukwuebuka <Lawrenceobi2@gmail.com>
2022-09-19 09:25:58 +0100

    doen

--- src/Controller/User/clearanceController.ts
+++ src/Controller/User/clearanceController.ts
@@ -2,10 +2,16 @@ import { Request, Response, NextFunction } from 'express'
 import db from '../../db'
 import { currentDate } from '../../utils'
 
-const libraryClearance = (req:Request,res:Response,next:NextFunction)=>{
+const libraryClearance = (req: Request, res: Response, next: NextFunction) => {
+    db.query("")
+}
+
+const medicalClearnce = (req: Request, res: Response, next: NextFunction) => {
 
 }
 
-const medicalClearnce = (req:Request,res:Response,next:NextFunction) =>{
-    
+
+export default {
+    libraryClearance,
+    medicalClearnce
 }
